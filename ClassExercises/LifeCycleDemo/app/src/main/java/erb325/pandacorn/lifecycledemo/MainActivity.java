15bfead4e5805f7741ae10f9c9cb9525611a5681
Ember Baker <erb39405@NH220-10.buad.bloomu.edu>
2015-09-01 14:18:09 -0400

    adding resourses

--- ClassExercises/LifeCycleDemo/app/src/main/java/erb325/pandacorn/lifecycledemo/MainActivity.java
+++ ClassExercises/LifeCycleDemo/app/src/main/java/erb325/pandacorn/lifecycledemo/MainActivity.java
@@ -1,6 +1,5 @@
 package erb325.pandacorn.lifecycledemo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
