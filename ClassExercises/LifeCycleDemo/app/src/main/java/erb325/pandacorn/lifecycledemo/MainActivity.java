6c7cbdedafa7d47ac2051591dad9822cff312213
Ember Baker <erb39405@NH220-10.buad.bloomu.edu>
2015-09-03 15:08:06 -0400

    debugging and logging in lifecycle app

--- ClassExercises/LifeCycleDemo/app/src/main/java/erb325/pandacorn/lifecycledemo/MainActivity.java
+++ ClassExercises/LifeCycleDemo/app/src/main/java/erb325/pandacorn/lifecycledemo/MainActivity.java
@@ -2,6 +2,7 @@ package erb325.pandacorn.lifecycledemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,11 +14,37 @@ public class MainActivity extends AppCompatActivity { //ActivityBar is deprecate
      * next time it is open.
      *
      */
+    private static String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "Created!");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "Started!");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG, "...Resumed!");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(LOG_TAG, "......paused!");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "...Stopped!");
     }
 
     /**
@@ -27,11 +54,14 @@ public class MainActivity extends AppCompatActivity { //ActivityBar is deprecate
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d(LOG_TAG, "options menu created");
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(LOG_TAG, "-options item selected");
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -41,7 +71,6 @@ public class MainActivity extends AppCompatActivity { //ActivityBar is deprecate
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
